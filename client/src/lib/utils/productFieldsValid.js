5df47bdef423f5824328050985821dc25d4621b9
Olga <41551585+olgashi@users.noreply.github.com>
2022-06-12 10:31:18 -0500

    Delete client directory

--- client/src/lib/utils/productFieldsValid.js
+++
@@ -1,5 +0,0 @@
-const productFieldsValid = (productObj) => {
-  return Object.keys(productObj).every(prop => String(productObj[prop]).length > 0 && String(productObj[prop]) !== 'NaN')
-}
-
-export default productFieldsValid;
